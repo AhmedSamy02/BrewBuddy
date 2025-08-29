52ee0accaa251c4b7d47c275c2eef6158a2e3b10
Zeina Ahmed Shenawy <zeinaelshenawy276@gmail.com>
2025-08-29 14:12:57 +0300

    Update CoffeeEntity and CoffeeDao with latest changes

---
+++ app/src/main/java/com/example/brewbuddy/data/local/database/entities/CoffeeEntity.kt
@@ -0,0 +1,16 @@
+package com.example.brewbuddy.data.local.database.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "coffees")
+data class CoffeeEntity(
+    @PrimaryKey val id: Int,
+    val title: String,
+    val description: String,
+    val imageUrl: String,
+    val ingredients: String,
+    val price: Double,
+    val category: String
+)
+
